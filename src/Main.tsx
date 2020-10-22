8e6fcac392dceee546360d0a92e47d22512ff1bf
swapnil.tiwari <swapnil.tiwari@flipkart.com>
2020-10-22 19:56:28 +0530

    Updating README.MD

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,9 @@
 
 import React from 'react';
-import { Easing, Text, View } from 'react-native';
+import { Easing, Text, View, StyleSheet, Image } from 'react-native';
 import { AnimationWrapperView } from './components/animation-wrapper/AnimationWrapperView';
 import { AnimationTriggerType } from './components/animation-wrapper/models/AnimationTriggerType';
 import { AnimationType } from './components/animation-wrapper/models/AnimationType';
-import { BaseAnimation } from './components/animation-wrapper/models/BaseAnimation';
 import { BounceAnimation } from './components/animation-wrapper/models/BounceAnimation';
 import { DraggableAnimation } from './components/animation-wrapper/models/DraggableAnimation';
 import { FadeInAnimation, FadeOutAnimation } from './components/animation-wrapper/models/FadeAnimation';
@@ -18,7 +17,7 @@ export default class Main extends React.Component {
         const bounceConfig: BounceAnimation = {
             type: AnimationType.BOUNCE,
             triggerType: AnimationTriggerType.ON_CLICK,
-            bounceHeight: 20,
+            bounceHeight: 30,
             animationDuration: 1000
         };
 
@@ -26,12 +25,12 @@ export default class Main extends React.Component {
             type: AnimationType.SCALE,
             triggerType: AnimationTriggerType.ON_CLICK,
             scaleDuration: 1000,
-            toScale: 3,
+            toScale: 2,
             easing: Easing.bounce
         };
         const rippleConfig: RippleAnimation = {
             type: AnimationType.RIPPLE,
-            triggerType: AnimationTriggerType.ON_CLICK,
+            triggerType: AnimationTriggerType.ON_LOAD,
             rippleColor: 'red',
             rippleCount: 5,
             rippleDuration: 1000,
@@ -46,15 +45,15 @@ export default class Main extends React.Component {
         const fadeInConfig: FadeInAnimation = {
             type: AnimationType.FADE_IN,
             triggerType: AnimationTriggerType.ON_LOAD,
-            animationDuration: 40000
+            animationDuration: 2000
         };
 
         const fadeOutConfig: FadeOutAnimation = {
             type: AnimationType.FADE_OUT,
             triggerType: AnimationTriggerType.ON_CLICK,
-            animationDuration: 400
+            animationDuration: 2000
         };
-        
+
         const slideInConfig: FadeInAnimation = {
             type: AnimationType.SLIDE_IN,
             triggerType: AnimationTriggerType.ON_LOAD,
@@ -68,13 +67,15 @@ export default class Main extends React.Component {
         };
         return (
             <View style={{
-                flexDirection: 'column', flex: 1,
-                justifyContent: 'space-evenly'
+                flexDirection: 'column',
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center'
             }}>
-                <AnimationWrapperView animationConfig={slideInConfig} animationDimen={{ width: 200, height: 200 }}>
-                    <Text style={{ backgroundColor: 'red', fontSize: 20, padding: 8, color: 'white' }}>Ripple Component</Text>
+                <AnimationWrapperView animationConfig={draggableConfig} animationDimen={{ width: 200, height: 200 }}>
+                    {this._renderCard()}
                 </AnimationWrapperView>
-
+                {/* 
                 <AnimationWrapperView animationConfig={slideOutConfig} animationDimen={{ width: 200, height: 200 }}>
                     <Text style={{ backgroundColor: 'blue', fontSize: 20, padding: 8, color: 'white' }}>Bounce Component</Text>
                 </AnimationWrapperView>
@@ -85,8 +86,40 @@ export default class Main extends React.Component {
 
                 <AnimationWrapperView animationConfig={draggableConfig} animationDimen={{ width: 200, height: 200 }}>
                     <Text style={{ backgroundColor: 'gray', fontSize: 20, padding: 8, color: 'white' }}>Draggable Component</Text>
-                </AnimationWrapperView>
+                </AnimationWrapperView> */}
+            </View>
+        );
+    }
+
+    _renderCard(): React.ReactNode {
+        return (
+            <View style={styles.container}>
+                <View style={{flex: 1, flexDirection: 'column'}}>
+
+                    <Text style={styles.paragraph}>
+                        This is a widget view
+                    </Text>
+                    <Text style={styles.paragraph}>
+                        Demo to apply transitions to this container
+                    </Text>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        height: 200,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#34495e'
+    },
+    paragraph: {
+        margin: 8,
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+});
